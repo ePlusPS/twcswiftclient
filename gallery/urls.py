184c64bda653fb0ba214825ff2e7508ee706bc7e
Gopalasivam <gopal@onecloudinc.com>
2015-01-26 18:15:23 +0530

    Removed extra files from repo

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,8 +5,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('gallery.views',
     url(r'^image/(?P<gallery_slug>[-\w]+)/$', 'gallery_page', name='smh_gallery_gallery'),
-    url(r'^image/(?P<gallery_slug>[-\w]+)/(?P<image_slug>[-\w]+)/$', 'image_page', name='smh_gallery_image'),
     url(r'^video/(?P<gallery_slug>[-\w]+)/$', 'video_gallery_page', name='smh_video_gallery'),
-    url(r'^video/(?P<gallery_slug>[-\w]+)/(?P<video_slug>[-\w]+)/$', 'video_page', name='smh_gallery_video')
 
 )
--- gallery/views.py
+++ gallery/views.py
@@ -14,15 +14,6 @@ def gallery_page(request, gallery_slug):
     images = Image.objects.filter(gallery=gallery_slug).order_by('-date')
     section = gallery_slug
     return (render(request, 'gallery.html', locals()))
-
-
-#View for a gallery item
-def image_page(request, gallery_slug, image_slug):
-    gallery = get_object_or_404(Gallery, slug=gallery_slug)  #Check for correct url
-    image = get_object_or_404(Image, slug=image_slug)
-    section = gallery_slug
-    permalink = request.build_absolute_uri()
-    return (render(request, 'image.html', locals()))
     
 #View for a gallery
 def video_gallery_page(request, gallery_slug):
@@ -33,14 +24,6 @@ def video_gallery_page(request, gallery_slug):
     section = gallery_slug
     return (render(request, 'video_gallery.html', locals()))
 
-
-#View for a gallery item
-def video_page(request, gallery_slug, video_slug):
-    gallery = get_object_or_404(Gallery, slug=gallery_slug)  #Check for correct url
-    video = get_object_or_404(Video, slug=video_slug)
-    section = gallery_slug
-    permalink = request.build_absolute_uri()
-    return (render(request, 'video.html', locals()))
 
 def static_section(request, section="home"):
     section = section
